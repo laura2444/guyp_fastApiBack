7cd6201a8a7ba9279ff62486ea3531e6e7e7dff5
Laura <lauracast142@gmail.com>
2025-08-17 09:36:40 -0500

    puerto

--- main.py
+++ main.py
@@ -41,5 +41,5 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 3000))
+    port = int(os.getenv("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
